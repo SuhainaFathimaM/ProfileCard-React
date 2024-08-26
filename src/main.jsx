15aac438565da6245324d41d10fc18cb7e98703a
SuhainaFathimaM <suhainafathimam@gmail.com>
2024-08-26 13:52:12 +0530

    Added files

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import './css/cards.css'
+
+import { Cards } from './Cards'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <App /> */}
+  <Cards />
+  </React.StrictMode>,
+)
